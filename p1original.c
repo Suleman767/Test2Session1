21625848e6af2c59aa84eac13fff9114c16b3e2d
Suleman767 <96038806+Suleman767@users.noreply.github.com>
2022-03-31 09:59:11 +0000

    p 1 original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,21 @@
-
+#include<stdio.h>
+#include<math.h>
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+  printf("enter the two numbers to find the distance between them");
+  scanf("%f%f%f%f",x1, y1, x2, y2);
+}
+void find_distance(float x1, float y1, float x2, float y2, float *s)
+{
+  *s =  (sqrt(x2-x1)*(x2-x1)+(y2-y2)*(y2-y1);
+}
+void output(float x1, float y1,float x2, float y2, float s)
+{
+ printf("the distance between between them is (%f%f)and 
+ (%f%f) is %f\n",x1, y1, x2, y2, s);
+}
+void main()
+{
+  int x1, y1, x2, y2, s;
+  input ()
 }
